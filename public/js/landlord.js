9abf64e1fab2971b4449821fe3009816948c3e0d
RubenRita <gustavo.rita65@gmail.com>
2018-01-25 00:58:43 +0100

    Hide/Show works fine, new field notifications

--- public/js/landlord.js
+++ public/js/landlord.js
@@ -1,43 +1,62 @@
-function showhide()
-      {
-          var div = document.getElementById("detail");
-          if (div.style.display !== "none") {
-              div.style.display = "none";
-          }
-          else {
-              div.style.display = "block";
-          }
-          $(detail).removeClass('hidden');
-          $(change_detail).addClass('hidden');
-          $(available).addClass('hidden');
-      }
+/*function showhide()
+{
+  var div = document.getElementById("detail");
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  }
+  else {
+    div.style.display = "block";
+  }
+  $(detail).removeClass('hidden');
+  $(change_detail).addClass('hidden');
+  $(available).addClass('hidden');
+  $(notification).addClass('hidden');
+}
 function showdetails()
 {
 
-    var div = document.getElementById("change_detail");
+  var div = document.getElementById("change_detail");
 
-    if (div.style.display !== "none") {
-        div.style.display = "none";
-    }
-    else {
-        div.style.display = "block";
-    }
-      $(change_detail).removeClass('hidden');
-      $(detail).addClass('hidden');
-      $(available).addClass('hidden');
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  }
+  else {
+    div.style.display = "block";
+  }
+  $(change_detail).removeClass('hidden');
+  $(detail).addClass('hidden');
+  $(available).addClass('hidden');
+  $(notification).addClass('hidden');
 }
 
 function showchange()
 {
-    var div = document.getElementById("available");
-    if (div.style.display !== "none") {
-        div.style.display = "none";
-    }
-    else {
-        div.style.display = "block";
-    }
+  var div = document.getElementById("available");
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  }
+  else {
+    div.style.display = "block";
+  }
 
-    $(available).removeClass('hidden');
-    $(change_detail).addClass('hidden');
-    $(detail).addClass('hidden');
+  $(available).removeClass('hidden');
+  $(change_detail).addClass('hidden');
+  $(detail).addClass('hidden');
+  $(notification).addClass('hidden');
+}
+
+function shownotification()
+{
+  var div = document.getElementById("notification");
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  }
+  else {
+    div.style.display = "block";
+  }
+
+  $(notification).removeClass('hidden');
+  $(available).addClass('hidden');
+  $(change_detail).addClass('hidden');
+  $(detail).addClass('hidden');
 }
