75b750daaaa3d3902df59b42e238aea425230c40
Luis Rocha <luis.rocha.jacinto@gmail.com>
2018-01-25 00:22:14 +0000

    Fixed the type of user default value.

--- database/migrations/2017_12_27_220606_add_type_to_users.php
+++ database/migrations/2017_12_27_220606_add_type_to_users.php
@@ -6,36 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddTypeToUsers extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-       Schema::table('users', function($table) {
-	  $table->boolean('type');
-	  $table->integer('landlord_id')->unsigned()->nullable();
-
-
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::table('users', function($table) {
+      $table->boolean('type')->default(false);
+      $table->integer('landlord_id')->unsigned()->nullable();
     });
 
+    Schema::table('users', function($table) {
+      $table->foreign('landlord_id')->references('landlord_id')->on('properties');
+    });
+  }
 
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
 
-
-
-        Schema::table('users', function($table) {
-          $table->foreign('landlord_id')->references('landlord_id')->on('properties');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-
-    }
+  }
 }
