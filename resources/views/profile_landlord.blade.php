5be34a239ef961353e18d9ac12bdcf26cf8d3006
Luis Rocha <luis.rocha.jacinto@gmail.com>
2018-01-28 02:04:14 +0000

    Changed the size of the icon. Added the notification colors

--- resources/views/profile_landlord.blade.php
+++ resources/views/profile_landlord.blade.php
@@ -14,6 +14,7 @@
 <link rel="stylesheet" href="{{ URL::asset('/css/profile/toastr/toastr.min.css')}}" />
 <link rel="stylesheet" href="{{ URL::asset('/css/profile/main.css')}}" />
 <link rel="stylesheet" href="{{ URL::asset('/css/profile/demo.css')}}" />
+<link rel="stylesheet" href="{{ URL::asset('/css/profile/profile.css')}}" />
 
 
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
@@ -48,7 +49,7 @@
 				</div>
 				<!-- logo -->
 				<div class="navbar-brand">
-					<a href="index.html">  <i class="fa fa-bed"></i> SHOMIE</a>
+          {{ config('app.name', 'shomie') }}
 				</div>
 				<!-- end logo -->
 				<div class="navbar-right">
@@ -56,129 +57,36 @@
 					<!-- navbar menu -->
 					<div id="navbar-menu">
 						<ul class="nav navbar-nav">
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle icon-menu" data-toggle="dropdown">
-									<i class="lnr lnr-alarm"></i>
-									<span class="notification-dot"></span>
-								</a>
-								<ul class="dropdown-menu notifications">
-									<li class="header"><strong>You have 7 new notifications</strong></li>
-									<li>
-										<a href="#">
-											<div class="media">
-												<div class="media-left">
-													<i class="fa fa-fw fa-flag-checkered text-muted"></i>
-												</div>
-												<div class="media-body">
-													<p class="text">Your campaign <strong>Holiday Sale</strong> is starting to engage potential customers.</p>
-													<span class="timestamp">24 minutes ago</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="media">
-												<div class="media-left">
-													<i class="fa fa-fw fa-exclamation-triangle text-warning"></i>
-												</div>
-												<div class="media-body">
-													<p class="text">Campaign <strong>Holiday Sale</strong> is nearly reach budget limit.</p>
-													<span class="timestamp">2 hours ago</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="media">
-												<div class="media-left">
-													<i class="fa fa-fw fa-bar-chart text-muted"></i>
-												</div>
-												<div class="media-body">
-													<p class="text">Website visits from Facebook is 27% higher than last week.</p>
-													<span class="timestamp">Yesterday</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="media">
-												<div class="media-left">
-													<i class="fa fa-fw fa-check-circle text-success"></i>
-												</div>
-												<div class="media-body">
-													<p class="text">Your campaign <strong>Holiday Sale</strong> is approved.</p>
-													<span class="timestamp">2 days ago</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="media">
-												<div class="media-left">
-													<i class="fa fa-fw fa-exclamation-circle text-danger"></i>
-												</div>
-												<div class="media-body">
-													<p class="text">Error on website analytics configurations</p>
-													<span class="timestamp">3 days ago</span>
-												</div>
-											</div>
-										</a>
-									</li>
-									<li class="footer"><a href="#" class="more">See all notifications</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle icon-menu" data-toggle="dropdown">
-									<i class="lnr lnr-cog"></i>
-								</a>
-								<ul class="dropdown-menu user-menu menu-icon">
-									<li class="menu-heading">ACCOUNT SETTINGS</li>
-									<li><a href="#"><i class="fa fa-fw fa-edit"></i> <span>Basic</span></a></li>
-									<li><a href="#"><i class="fa fa-fw fa-bell"></i> <span>Notifications</span></a></li>
-									<li><a href="#"><i class="fa fa-fw fa-sliders"></i> <span>Preferences</span></a></li>
-									<li><a href="#"><i class="fa fa-fw fa-lock"></i> <span>Privacy</span></a></li>
-									<li class="menu-heading">BILLING</li>
-									<li><a href="#"><i class="fa fa-fw fa-file-text-o"></i> <span>Invoices</span></a></li>
-									<li><a href="#"><i class="fa fa-fw fa-credit-card"></i> <span>Payments</span></a></li>
-									<li><a href="#"><i class="fa fa-fw fa-refresh"></i> <span>Renewals</span></a></li>
-									<li class="menu-button">
-										<a href="#" class="btn btn-primary"><i class="fa fa-rocket"></i> UPGRADE PLAN</a>
-									</li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle icon-menu" data-toggle="dropdown">
-									<i class="lnr lnr-question-circle"></i>
-								</a>
-								<ul class="dropdown-menu user-menu">
-									<li>
-										<form class="search-form help-search-form">
-											<input value="" class="form-control" placeholder="How can we help?" type="text">
-											<button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
-										</form>
-									</li>
-									<li class="menu-heading">HOW-TO</li>
-									<li><a href="#">Setting up Campaign</a></li>
-									<li><a href="#">Understanding Website Analytics</a></li>
-									<li><a href="#">Boost Your Sales</a></li>
-									<li><a href="#">Knowing Your Audience</a></li>
-									<li class="menu-heading">ACCOUNT</li>
-									<li><a href="#">Change Password</a></li>
-									<li><a href="#">Privacy &amp; Security</a></li>
-									<li><a href="#">Membership</a></li>
-									<li class="menu-heading">BILLING</li>
-									<li><a href="#">Setup Payment</a></li>
-									<li><a href="#">Auto-Renewal Program</a></li>
-									<li><a href="#">Cancellation</a></li>
-									<li class="menu-button">
-										<a href="#" class="btn btn-primary"><i class="fa fa-question-circle"></i> HELP CENTER</a>
-									</li>
-								</ul>
-							</li>
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i> <span class="badge pending-requests">5</span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#"><i class="fa fa-fw fa-check accepted"></i> Visit at 12/04/2018 at 12:20. </a></li>
+                  <li><a href="#"><i class="fa fa-fw fa-times rejected"></i> Visit at 12/05/2018 at 15:20. </a></li>
+                  <li><a href="#"><i class="fa fa-fw fa-clock-o pending"></i> Visit at 12/05/2018 at 15:20. </a></li>
+                  <li><a href="#"><i class="fa fa-fw fa-times rejected "></i> Visit at 13/05/2018 at 16:20. </a></li>
+                  <li><a href="#"><i class="fa fa-fw fa-check accepted"></i> Visit at 11/05/2018 at 15:20. </a></li>
+                </ul>
+              </li>
+              <li class="dropdown hidden-xs">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                  {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+
+                <ul class="dropdown-menu" role="menu">
+                  <li>
+                    <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    Logout
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                  </form>
+                </li>
+              </ul>
+              </li>
 						</ul>
 					</div>
 					<!-- end navbar menu -->
@@ -196,7 +104,7 @@
 				<div class="user-account">
 					<img src="/img/default.png" class="img-responsive img-circle user-photo" alt="User Profile Picture">
 					<div class="dropdown">
-						<a href="#" class="dropdown-toggle user-name" data-toggle="dropdown">Hello, <strong>Austin Hoffman</strong> <i class="fa fa-caret-down"></i></a>
+						<a href="#" class="dropdown-toggle user-name" data-toggle="dropdown">Hello, <strong>{{ Auth::user()->name }}</strong> <i class="fa fa-caret-down"></i></a>
 						<ul class="dropdown-menu dropdown-menu-right account">
 							<li><a href="#">My Profile</a></li>
 							<li><a href="#">Messages</a></li>
@@ -208,7 +116,7 @@
 				</div>
 				<nav id="left-sidebar-nav" class="sidebar-nav">
 					<ul id="main-menu" class="metismenu">
-						<li class="active"><a href="index.html"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+						<li class="active"><a href="index.html"><i class="lnr lnr-home"></i> <span>Menu Principal</span></a></li>
 						<li class="">
 							<a href="#uiElements" class="has-arrow" aria-expanded="false"><i class="lnr lnr-magic-wand"></i> <span>UI Elements</span></a>
 							<ul aria-expanded="true">
