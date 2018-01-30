10033897798c469c25b3f77406598821ea658695
RubenRita <gustavo.rita65@gmail.com>
2018-01-30 01:24:45 +0100

    Status, change code to have better presentation

--- resources/views/landlord/notifications.blade.php
+++ resources/views/landlord/notifications.blade.php
@@ -124,36 +124,37 @@
 											<td>  <a href="{{ route('property', ['id'=> $communication->property_id]) }}" target="_blank">{{ $communication->property_id }}</a></td>
 											<td>{{$communication->visit_date}}</td>
 											<td>{{$communication->visit_time}}</td>
-											@if($communication->state == 0)
-											<td>Pendente</td>
-											@elseif($communication->state == 1)
-											<td>Aceite</td>
-											@else
-											<td>Rejeitado</td>
-											@endif
-
+											<td>
+												@if($communication->state == 0)
+												Pendente
+												@elseif($communication->state == 1)
+												Aceite
+												@else
+												Rejeitado
+												@endif
+											</td>
 											<td>
 												<span type="button" style="cursor:pointer;padding-right:10px;"><i class="fa fa-fw fa-check accepted"></i> </span>
 												<span style="cursor:pointer;"><i class="fa fa-fw fa-times rejected"></i>  </span>
 											</td>
-												</tr>
-												@endforeach
-											</tbody>
-										</table>
-									</div>
-
-								</div>
+										</tr>
+										@endforeach
+									</tbody>
+								</table>
 							</div>
-
 
 						</div>
 					</div>
+
+
 				</div>
 			</div>
-			<div class="clearfix"></div>
-			<footer>
-				<p class="copyright">&copy; 2018 Shomie</p>
-			</footer>
 		</div>
+	</div>
+	<div class="clearfix"></div>
+	<footer>
+		<p class="copyright">&copy; 2018 Shomie</p>
+	</footer>
+</div>
 
-		@endsection
+@endsection
